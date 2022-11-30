a30c476cf41d32a52909bd4cb42dc9df58526b87
George <gnjuki19@gmail.com>
2022-11-30 16:45:50 +0300

    Fixing the search component for getting data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
+import axios from "axios";
 import React, { useState } from "react";
 function App() {
+  const [data, setData] = useState({});
 
-  const [data, setData] = useState({})
+  const [location, setLocation] = useState("");
 
-  const [location, setLocation] = useState("")
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=146dfe8931b293b83b1f72db06ef1d0c`;
 
-  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=146dfe8931b293b83b1f72db06ef1d0c`
+  // const searchLocation = (e) => {
+  //   if (e.key === "Enter") {
+  //     axios.get(URL).then((res) => {
+  //       setData(res.data);
+  //       console.log(res.data);
+  //     });
+  //     setLocation("");
+  //   }
+  // };
+
+  const getLocation = (e) => {
+    fetch(URL)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data)
+        console.log(data)
+      });
+      setLocation("")
+  };
+
   return (
     <div className="app">
+      <div className="search">
+        <input
+          type="text"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+          placeholder="ENter a city"
+          onKeyDown={getLocation}
+        />
+      </div>
       <div className="container">
         <div className="top">
           <div className="location">
