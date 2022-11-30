b050c08e100c0b1efe76f24dcbb6dc6b52c78407
George <gnjuki19@gmail.com>
2022-11-30 16:01:50 +0300

    Made the html structure for the app and added background image

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-
+import React, { useState } from "react";
 function App() {
+
+  const URL = "https://api.openweathermap.org/data/2.5/weather?q=nairobi&appid=146dfe8931b293b83b1f72db06ef1d0c"
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <h3>Nairobi</h3>
+          </div>
+          <div className="temp">
+            <h1>65° celcius</h1>
+          </div>
+          <div className="description">
+            <p>Cloudy with a chance of meatballs</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>65° celcius</p>
+          </div>
+          <div className="humidity">
+            <p>20%</p>
+          </div>
+          <div className="wind">
+            <p>20kmh</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
